f882b4e11f03d3fa4cd4bea326d98a6f0e1a1368
kenkelbt <kenkelbt@mail.uc.edu>
2022-10-11 13:53:24 -0400

    finished latitude and longitude

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,14 +1,14 @@
-const latitude = document.getElementById("latitude");
-const longitude = document.getElementById("longitude");
+const city = document.getElementById("city");
 
 if("geolocation" in navigator) {
     console.log("geolocation available");
     navigator.geolocation.getCurrentPosition(position => {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
-        latitude.textContent = lat;
-        longitude.textContent = lon;
+        console.log(`Latitude: ${lat}`)
+        console.log(`Longitude: ${lon}`)
     })
 } else {
     console.log("geolocation not available");
 }
+
